8ef2b1d7e1b52cd8e316f3e187ccd3ba4f790b28
MikeZorin <mykhailo.zorin@gmail.com>
2020-10-19 12:38:08 -0500

    Add project files.

---
+++ DotNetCore2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotNetCore2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
