b1e90669d9691c940241337c8a9bdb1ca3e26896
MikeZorin <mykhailo.zorin@gmail.com>
2020-10-26 14:31:49 -0500

    added lesson 006

--- DotNetCore2/Program.cs
+++ DotNetCore2/Program.cs
@@ -1,62 +1,122 @@
-﻿using MySql.Data.MySqlClient;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using System.Text.Json;
 
 namespace DotNetCore2
 {
     class Program
     {
+        private static void AddText(FileStream fs, string value)
+        {
+            byte[] info = new UTF8Encoding(true).GetBytes(value);
+            fs.Write(info, 0, info.Length);
+        }
+
         static void Main(string[] args)
         {
-            string input = @"I do not like them 
-In a house.
-I do not like them
-With a mouse.
-I do not like them
-Here or there.
-I do not like them
-Anywhere.
-I do not like green eggs and ham.
-I do not like them, Sam - I - am.";
-
-            // Console.WriteLine(input);
-
-            StringBuilder sb = new StringBuilder(input);
-
-            sb.Replace("not", string.Empty);
-            sb.Replace("  ", " ");
-
-            // Console.WriteLine(sb.ToString());
-
-
-
-            string cs = @"server=127.0.0.1;userid=root;password=;database=sql_classes";
-
-            using (MySqlConnection conn = new MySqlConnection(cs))
+            
+            string path = @"d:\temp\MyTest.txt";
+            /*
+            //Create the file.
+            using (FileStream fs = File.Create(path))
             {
-                MySqlCommand cmd = new MySqlCommand();
-                cmd.CommandText = "select count(*) from Products";
-                cmd.Connection = conn;
-
-                conn.Open();
-
-                var count = Int32.Parse( cmd.ExecuteScalar().ToString() );
-
-                Console.WriteLine(count);
-
-
-                // cmd.CommandText = @"insert into Products(title, price, qtyAvailable) values
-                //                        ('Calculator', 5, 777')";
-
-                //cmd.CommandText = @"update Products set qtyAvailable=1024 "; // where id=4
+                AddText(fs, "Hello C# Files");
                 
-                cmd.CommandText = @"delete from Products where id=4"; // 
-                
-                count = Int32.Parse( cmd.ExecuteNonQuery().ToString() );
+            }
+            */
+            File.AppendAllText(path, "Hello Files\n");
+            File.AppendAllText(path, "Hello Files2\n");
 
-                Console.WriteLine(count + " row(s) inserted" );
+            /*
+            using (FileStream fs = File.OpenRead(path))
+            {
+                byte[] b = new byte[1024];
+                UTF8Encoding temp = new UTF8Encoding(true);
+                
+                while (fs.Read(b, 0, b.Length) > 0)
+                {
+                    Console.WriteLine(temp.GetString(b));
+                }
+            }
+            */
+
+            Console.WriteLine(File.ReadAllText(path));
+
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+
+
+
+            string jsonString = string.Empty;
+
+            jsonString = JsonSerializer.Serialize(Student.LoadData());
+            // jsonString = JsonSerializer
+            //     .Serialize(new Student() { Id = 777, Name = "John", TotalMarks = 222 });
+            //                             // {"Id":777,"Name":"John","TotalMarks":222}
+            Console.WriteLine(jsonString);
+
+            path = @"d:\temp\json.txt";
+            File.AppendAllText(path, jsonString);
+
+
+            var res = JsonSerializer.Deserialize<List<Student>>(jsonString);
+
+            Console.WriteLine("=============================================================");
+
+            foreach (var s in res)
+            {
+                Console.WriteLine(s.Id + " " + s.Name + " " + s.TotalMarks);
+            }
+
+            Console.WriteLine("=============================================================");
+
+            path = @"d:\temp\text.json";
+            // C:\Users\mykha\source\repos\DotNetCore2\DotNetCore2
+
+            jsonString = File.ReadAllText(path);
+
+            Console.WriteLine(jsonString);
+
+            var res2 = JsonSerializer.Deserialize<Top>(jsonString);
+            
+            Console.WriteLine("=============================================================");
+
+            Console.WriteLine(res2.menu.header);
+            foreach (var i in res2.menu.items)
+            {
+                if (i != null)
+                {
+                    Console.Write("\t id = " + i.id);
+
+                    if (i.label != null)
+                        Console.Write("; label = " + i.label);
+
+                    Console.WriteLine();
+                }
+                else
+                    Console.WriteLine("\t null");
             }
         }
+    }
+
+    class Item
+    {
+        public string id { set; get; }
+        public string label { set; get; }
+    }
+
+    class Menu
+    {
+        public string header { set; get; }
+        public List<Item> items { set; get; }
+    }
+
+    class Top
+    {
+        public Menu menu { set; get; }
     }
 }
---
+++ DotNetCore2/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNetCore2
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int TotalMarks { get; set; }
+
+        public static List<Student> LoadData()
+        {
+            List<Student> list = new List<Student>();
+
+            list.Add(new Student() { Id = 101, Name = "Lucy", TotalMarks = 1020 });
+            list.Add(new Student() { Id = 102, Name = "Mark", TotalMarks = 1008 });
+            list.Add(new Student() { Id = 103, Name = "Rosy", TotalMarks = 1025 });
+
+            return list;
+        }
+    }
+}
