8afff50ce52c6b96f0e59488148c500f8d4f2a09
MikeZorin <mykhailo.zorin@gmail.com>
2020-10-29 14:17:44 -0500

    added lesson 009

---
+++ Cripto/Cripto.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Cripto
+{
+    public class Cripto
+    {
+		public static string GenerateSecretString()
+		{
+			Random rnd = new Random();
+			string res = string.Empty;
+
+			for (int i = 1; i <= 32; i++)
+			{
+				res += (char)rnd.Next(48, 122);
+			}
+
+			return res;
+		}
+
+		public static string EncryptString(string key, string plainText)
+		{
+			byte[] iv = new byte[16];
+			byte[] array;
+
+			using (Aes aes = Aes.Create())
+			{
+				aes.Key = Encoding.UTF8.GetBytes(key);
+				aes.IV = iv;
+
+				ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+				using (MemoryStream memoryStream = new MemoryStream())
+				{
+					using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, encryptor, CryptoStreamMode.Write))
+					{
+						using (StreamWriter streamWriter = new StreamWriter((Stream)cryptoStream))
+						{
+							streamWriter.Write(plainText);
+						}
+
+						array = memoryStream.ToArray();
+					}
+				}
+			}
+
+			return Convert.ToBase64String(array);
+		}
+
+		public static string DecryptString(string key, string cipherText)
+		{
+			byte[] iv = new byte[16];
+			byte[] buffer = Convert.FromBase64String(cipherText);
+
+			using (Aes aes = Aes.Create())
+			{
+				aes.Key = Encoding.UTF8.GetBytes(key);
+				aes.IV = iv;
+				ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+				using (MemoryStream memoryStream = new MemoryStream(buffer))
+				{
+					using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, decryptor, CryptoStreamMode.Read))
+					{
+						using (StreamReader streamReader = new StreamReader((Stream)cryptoStream))
+						{
+							return streamReader.ReadToEnd();
+						}
+					}
+				}
+			}
+		}
+
+		public static string toMD5(string s)
+		{
+			using (var provider = MD5.Create())
+			{
+				StringBuilder builder = new StringBuilder();
+
+				foreach (byte b in provider.ComputeHash(Encoding.UTF8.GetBytes(s)))
+					builder.Append(b.ToString("x2").ToLower());
+
+				return builder.ToString();
+			}
+		}
+
+		public static string SaltAndHash(string str)
+		{
+			// generate a 128-bit salt using a secure PRNG
+			byte[] salt = new byte[128 / 8];
+			using (var rng = RandomNumberGenerator.Create())
+			{
+				rng.GetBytes(salt);
+			}
+			// Console.WriteLine($"Salt: {Convert.ToBase64String(salt)}");
+
+			// derive a 256-bit subkey (use HMACSHA1 with 10,000 iterations)
+			string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+				password: str,
+				salt: salt,
+				prf: KeyDerivationPrf.HMACSHA1,
+				iterationCount: 10000,
+				numBytesRequested: 256 / 8));
+			// Console.WriteLine($"Hashed: {hashed}");
+
+			return hashed;
+
+		}
+	}
+}
--- DotNetCore2/Program.cs
+++ DotNetCore2/Program.cs
@@ -1,13 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text;
-using System.Text.Json;
-using System.Linq;
-using System.Xml.Linq;
-using System.Xml.Serialization;
-using System.Xml;
-using System.Security.Cryptography;
+using Cripto;
+using MySql.Data.MySqlClient;
 
 namespace DotNetCore2
 {
@@ -15,131 +8,49 @@ namespace DotNetCore2
     {
         static void Main(string[] args)
         {
-            string path2 = "Menu2.xml";
-            if (File.Exists("Menu2.xml"))
+
+            string str = "123456";
+            Console.WriteLine(Cripto.Cripto.toMD5(str));
+
+            string cs =
+                @"server=127.0.0.1;userid=root;password=;database=sql_classes";
+
+            using (var conn = new MySqlConnection(cs))
             {
-                Console.WriteLine("The file exists!");
-            }
+                var cmd = new MySqlCommand("SELECT username, password from Passwords", conn);
 
-            string path = @"c:\programs\file.txt";
+                conn.Open();
+                MySqlDataReader rdr = cmd.ExecuteReader();
 
-            Console.WriteLine(Path.GetFullPath(path2));
-            Console.WriteLine(Path.GetFileName(path));
-            Console.WriteLine(Path.GetFileNameWithoutExtension(path) + ".xml");
-            Console.WriteLine(Path.GetExtension(path));
-
-            Console.WriteLine("==============================================");
-
-            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-
-            // IEnumerable<int> res = numbers.Where(n => n % 2 == 0).Select(n => n);
-
-            var res = numbers.Where(n => n % 2 == 0);//.Select(n => n);
-
-            var res2 = from i in numbers
-                       where i % 2 == 0
-                       select i;
-
-            var list1 = Student.LoadData().Where(s => s.TotalMarks > 1010)
-                                .Select(s => s.Id + " " + s.Name + " " + s.TotalMarks);
-
-            var list2 = from s in Student.LoadData()
-                        where s.TotalMarks > 1010
-                        select s;
-
-            foreach (var s in res)
-            {
-                Console.WriteLine(s);
-            }
-
-            Console.WriteLine( "=======================================" );
-            Console.WriteLine( numbers.Max() );
-            Console.WriteLine( numbers.Min() );
-
-            Console.WriteLine( numbers.Where(n => n % 2 == 0).Min() );
-            Console.WriteLine( numbers.Where(n => n % 2 == 0).Max() );
-
-            Console.WriteLine(numbers.Sum());
-            Console.WriteLine(numbers.Count());
-            Console.WriteLine(numbers.Average());
-
-            string[] countries = { "Canada", "Usa", "Uk", "India", "Ukraine" };
-            Console.WriteLine(countries.Min( c => c.Length ));
-            Console.WriteLine(countries.Max( c => c.Length));
-
-            var ls = new List<Shape>
-            {
-                new Circle { Color = "Red", R = 2.5},
-                new Rectangle { Color = "Blue", H = 20, W = 10 },
-                new Circle { Color = "Green", R = 8},
-                new Circle { Color = "Grey", R = 12.3},
-                new Rectangle { Color = "Blue", H = 45, W = 18 }
-            };
-
-            foreach (var s in ls)
-            {
-                Console.WriteLine( s.GetType().Name + " " +  s.Color + " " + s.Area);
-            }
-
-            var xs = new XmlSerializer( typeof(List<Shape>) );
-            string pathXML = "Shapes.xml";
-
-            using (FileStream fs = File.Create(pathXML))
-            {
-                xs.Serialize(fs, ls);
-            }
-
-
-            using (StringReader sr = new StringReader( File.ReadAllText( pathXML ) ))
-            {
-                var res22 = xs.Deserialize(sr) as List<Shape>;
-
-                foreach (var s in res22)
+                while (rdr.Read())
                 {
-                    Console.WriteLine(s.Color + " " + s.Area);
+                    // Console.WriteLine( rdr.GetString(0) + " " + rdr.GetString(1));
+
+                    Console.WriteLine(
+                            Cripto.Cripto.toMD5(str) == rdr.GetString(1)
+                        );
                 }
+
+                Console.WriteLine("Salted hashes: " + Cripto.Cripto.SaltAndHash(str));
+
+                //Console.WriteLine(Cripto.Cripto.GenerateSecretString());
+                
+                
+                Console.WriteLine( (int)'a' );
+                Console.WriteLine( (int)'0');
+                Console.WriteLine( (int)'z');
+
+                string ccn = "1234-5678-9012-3456";
+                string sk = Cripto.Cripto.GenerateSecretString();
+
+                string encCcn = Cripto.Cripto.EncryptString(sk, ccn);
+
+                Console.WriteLine(encCcn);
+
+                Console.WriteLine( Cripto.Cripto.DecryptString(sk, encCcn) );
+
+
             }
         }
-    }
-
-    [XmlInclude(typeof(Circle))]
-    [XmlInclude(typeof(Rectangle))]
-    public class Shape
-    {
-        public string Color { get; set; }
-        public virtual double Area { get; }
-    }
-
-    public class Circle : Shape
-    {
-        public double R { set; get; }
-        public override double Area => Math.PI * Math.Pow(R, 2);
-    }
-
-    public class Rectangle : Shape
-    {
-        public double H { set; get; }
-        public double W { set; get; }
-        public override double Area => H * W;
-    }
-
-
-
-
-    public class Item
-    {
-        public string id { set; get; }
-        public string label { set; get; }
-    }
-
-    public class Menu
-    {
-        public string header { set; get; }
-        public List<Item> items { set; get; }
-    }
-
-    public class Top
-    {
-        public Menu menu { set; get; }
     }
 }
